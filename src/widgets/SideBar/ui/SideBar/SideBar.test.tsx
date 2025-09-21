ae5b9250905a5990eee042659d994c417a71e9de
Broker2007 <fedorovpavel0007@yandex.ru>
2025-09-21 15:38:01 +0300

    Настройка тестов для sidebar - componentrender

--- src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
@@ -1,16 +1,16 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import { withTranslation } from 'react-i18next';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 // eslint-disable-next-line max-len
 import { renderWithTranslation } from '../../../../shared/lib/tests/renderWithTranslation/renderWithTranslation';
 import SideBar from './SideBar';
 
 describe('SideBar', () => {
     test('test render', () => {
-        renderWithTranslation(<SideBar />);
+        componentRender(<SideBar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('test render', () => {
-        renderWithTranslation(<SideBar />);
+        componentRender(<SideBar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
